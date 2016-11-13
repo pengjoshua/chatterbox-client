bd450c63a4b21e01c91adafaec1021e18456bd7d
pengjoshua <joshuapeng7@gmail.com>
2016-11-12 16:20:05 -0800

    Complete requirements, chatterbox fully working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,14 +1,27 @@
 // YOUR CODE HERE:
 var app = {
+  escape: function(s) {
+    return String(s).replace(/[&<>'"]/g, function(c) {
+      return escapeObj[c];
+    });
+  },
 
   init: function() {
-    friends = [];
+    escapeObj = {
+      '&': '&amp',
+      '<': '&lt',
+      '>': '&gt',
+      '/"': '&quot',
+      "/'": '&#x27',
+      '//': '&#x2F'
+    };
+
+    friends = {};
     userName = '';
     text = '';
     roomName = '';
-    uniqRooms = [];
+    uniqRooms = {};
 
-    messages = [];
     $chats = $('#chats');
     $room = $('#roomSelect');    // getting id and setting it
     $main = $('#main');
@@ -18,6 +31,7 @@ var app = {
 
     $('#send').on('submit', function(event) {
       app.handleSubmit($('#message'));
+      $('#message').val('');
       event.preventDefault();
     });
 
@@ -25,10 +39,16 @@ var app = {
       var room = $('#roomSelect :selected').val();
       app.filterRoom(room);
     });
+
+    $('#roomSend').on('submit', function(event) {
+      app.renderRoom($('#roomCreate').val());
+      $('#roomCreate').val('');
+
+      event.preventDefault();
+    });
   },
 
   send: function(message) {
-    console.log('message submitted from submit button', message);
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: 'https://api.parse.com/1/classes/messages',
@@ -36,7 +56,6 @@ var app = {
       data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
-        console.log('chatterbox: Message sent', data);
 
       },
       error: function (data) {
@@ -51,12 +70,11 @@ var app = {
       url: 'https://api.parse.com/1/classes/messages',
       type: 'GET',
       contentType: 'application/json',
-      data: 'order=-createdAt',
+      // data: 'order=-createdAt',
       success: function (data) {
         data.results.sort(function(a, b) {
           return a.createdAt > b.createdAt ? -1 : 1;
         });
-        console.log('chatterbox: Message fetched', data);
         _.each(data.results, (message) => {
           setTimeout(app.renderMessage(message), 1000);
         });
@@ -75,24 +93,32 @@ var app = {
 
   renderMessage: function(message) {
     userName = message.username;
-    text = message.text;
+    text = app.escape(message.text);
     roomName = message.roomname;
 
-    $userName = $('<span class="username"> ' + userName + '</span>');
-    $message = $('<span class="tweet">: ' + text + ' </span> </br>');
-    $roomName = $('<option> ' + roomName + ' </option>');
+    $userName = $('<span class="username"></span>');
+    $userName.text(userName + ': ');
+    $message = $('<span class="tweet">: </span>');
+    $message.text(text);
+    $roomName = $('<option> </option>');
+    $roomName.text(roomName);
+    $messageDiv = $('<div class="messages"> </div>');
 
-    if (!_.contains(uniqRooms, roomName)) {
-      uniqRooms.push(roomName);
-      $('#roomSelect').append($roomName);
-      var $divRoom = $('<div class=' + roomName + '> </div>');
-      $chats.append($divRoom);
-    }
-
+    $messageDiv.append($userName);
     $userName.append($message);
 
+    if (!_.contains(uniqRooms, roomName)) {
+      uniqRooms[roomName] = roomName;
+      $('#roomSelect').append($roomName);
+      var $divRoom = $('<div class="' + roomName + ' divRoom"' + '> </div>');
+      $divRoom.fadeIn();
+      $chats.append($divRoom);
+
+    }
+
+
     var truthy = false;
-    _.each($chats.children('.' + roomName).children(), function(tweet) {
+    _.each($chats.children('.' + roomName).children().children(), function(tweet) {
       if (($userName.className === $(tweet).className) && 
         ($(tweet).children().text() === $userName.children().text())) {
         truthy = true;
@@ -100,7 +126,8 @@ var app = {
     });
 
     if (!truthy) {
-      $chats.children('.' + roomName).prepend($userName);
+      $messageDiv.fadeIn();
+      $chats.children('.' + roomName).prepend($messageDiv);
     }
 
     
@@ -114,38 +141,60 @@ var app = {
   },
 
   renderRoom: function(roomName) {
-    $room.append($roomName);
+    if (!_.contains(uniqRooms, roomName)) {
+      uniqRooms[roomName] = roomName;
+      var $newRoom = $('<option> ' + roomName + '</option>');
+      $room.append($newRoom);
+    }
   },
 
   handleUsernameClick: function(user) {
-    friends.push(user);
+    var truthy = false;
+    var $friendsHTML = $('#friends');
+
+    if ($('#friends').children().length === 0) {
+      var $newFriend = $('<li>' + user + '</br></li>');
+      $friendsHTML.append($newFriend);
+      truthy = true;
+      $userName.addClass('friend');
+      friends[user] = user;
+    } else {
+      if (_.contains(friends, user)) {
+        truthy = true;
+      }
+      friends[user] = user;
+
+      _.each($chats.children().children().children(), function(userSpan) {
+        var text = $(userSpan.firstChild).text().replace(' ', '');
+        if (_.contains(friends, text)) {
+          $(userSpan).addClass('friend');
+        }
+      });
+    }
+
+    if (!truthy) {
+      $friendsHTML.append('<li>' + user + '</li>');
+    }
   },
 
   handleSubmit: function(submitValNode) {
     var message = {
       username: window.location.search.split('=')[1],
       text: submitValNode.val(),
-      roomname: $('#roomSelect :selected').text().trim()
+      roomname: $('#roomSelect :selected').text()
     };
     app.send(message);
   },
 
   filterRoom: function(room) {
-    var $allChats = $('#chats');
-    console.log('filtering with', room);
-    _.each($allChats.children(), function(divs) {
-      if ($(divs).hasClass(room)) {
-        console.log('WORKING');
-        $(divs).show();
+    _.each($chats.children(), function(divs) {
+      if ($(divs).hasClass(room) || $(divs).hasClass('messageTitle')) {
+        $(divs).fadeIn();
       } else {
-        $(divs).hide();
+        $(divs).fadeOut();
       }
     });
   }
-    // _.each($chatNodes, (chat) => {
-    //   if (chat.className !== room.text()) {
-    //     $('.').toggle();
-    //   }
-    // });
+
 };
 
